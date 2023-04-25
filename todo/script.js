1e41659ced8f5e3caa08a21a69bbe9e67dda5942
SaTliX <a.santarelli74@gmail.com>
2023-04-25 20:20:50 +0200

    homepage

--- todo/script.js
+++ todo/script.js
@@ -1,6 +1,6 @@
 const defaultTodo = [
-    {done: true, name: "Coucou"},
-    {done: true, name: "Coucou"},
+    {done: true, name: "tache1"},
+    {done: true, name: "tache2"},
 ]
 
 const displayTodos = (listTodo) => {
@@ -9,9 +9,19 @@ const displayTodos = (listTodo) => {
 
 
 // #1 selectionner le bouton avec l'id #add, le container #container-list, le input #new
+const btn= document.querySelector('#add')
+const container= document.querySelector('#containe-list')
+const input = document.querySelector('#new')
 
 
 // #2 Afficher les todos dans le container avec une fonction displayTodos
+
+console.log(defaultTodo)
+const displayTodo(list)
+defaultTodo.forEach(task => {
+    container.innerHTML += '<div class="p-2 bg-slate-700 rounded mt-2">${task.name}</div>'
+    
+});
 
 
 // #3 Ajouter un event listener sur le bouton pour ajouter un todo
